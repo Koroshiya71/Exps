0e8afb1dc56ec964bb3d4eec76a90fb16ab5b62d
Koroshiya71 <Koroshiya991125@gmail.com>
2020-10-25 15:16:24 +0800

    add Unity Api part 6 and 7

---
+++ C# Study/Unity Api/API06MonoBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API06MonoBehaviour : MonoBehaviour
+{
+    public Attack atk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print(isActiveAndEnabled);
+        //print(enabled);
+        //enabled = false;
+        //print(name);
+        //print(tag);
+        //print(gameObject);
+        //print(transform);
+
+        //print("the World" );
+
+        //print(atk.isActiveAndEnabled);
+        //print(atk.enabled);
+        //atk.enabled = false;
+        //print(atk.name);
+        //print(atk.tag);
+        //print(atk.gameObject);
+        //print(atk.transform);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ C# Study/Unity Api/API07Invoke.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API07Invoke : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invoke(nameof(Attack),3);
+        InvokeRepeating(nameof(Attack),4,2);
+        CancelInvoke(nameof(Attack));
+    }
+
+    void Attack()
+    {
+        print("Attacking");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        bool res = IsInvoking(nameof(Attack));
+        print(res);
+    }
+}
