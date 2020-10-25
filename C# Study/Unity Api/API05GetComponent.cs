0e77fdf275408b3fe2367e70b478420c88ea7953
Koroshiya71 <Koroshiya991125@gmail.com>
2020-10-25 12:12:05 +0800

    添加Unity API学习-05

---
+++ C# Study/Unity Api/API05GetComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API05GetComponent : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Attack atk=target.GetComponent<Attack>();
+        Transform t = target.GetComponent<Transform>();
+
+        print(t);
+        print(atk);
+        print("-----------------");
+        Attack[] atks = target.GetComponents<Attack>();
+        print(atks.Length);
+        print("-----------------");
+
+        atks = target.GetComponentsInChildren<Attack>();
+        foreach (Attack a in atks)
+        {
+            print(a);
+        }
+        print("-----------------");
+        atks = target.GetComponentsInParent<Attack>();
+        foreach (var a in atks)
+        {
+         print(a);   
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ C# Study/Unity Api/Attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Test()
+    {
+        print("Test");
+    }
+    void Update()
+    {
+        
+    }
+}
