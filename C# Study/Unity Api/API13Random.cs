b9969aa15bb8a347dd885e609d16f22dc6e759be
Koroshiya71 <Koroshiya991125@gmail.com>
2020-11-04 12:16:15 +0800

    添加Unity Api13Random和14四元数Quatrernion

---
+++ C# Study/Unity Api/API13Random.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API13Random : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print(Random.Range(233,777));//不包含最大值
+        //print(Random.Range(0,1f));
+        Random.InitState((int)System.DateTime.Now.Ticks);//如果使用同一种子，则随机数序列永远相同
+        //即使不手动设置随机数种子，unity依旧会每次初始化时确保随机数序列不同
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            print(Random.Range(4,100));
+    }
+}
---
+++ C# Study/Unity Api/API14Quaternion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class API14Quaternion : MonoBehaviour
+{
+    [FormerlySerializedAs("cube")] public Transform player;
+
+    public float rotSpeed = 1;
+    public Transform enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //print(player.eulerAngles);
+        //var rotation = player.rotation;
+        //print(rotation);
+
+        //player.eulerAngles=new Vector3(45,45,45);//两者等效，四元数和欧拉角可以互相转换
+        //rotation = Quaternion.Euler(new Vector3(45, 45, 45));
+        //player.rotation = rotation;
+        //print(rotation.eulerAngles);
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = enemy.position - player.position;
+        dir.y = 0;
+        Quaternion target = Quaternion.LookRotation(dir);
+        player.rotation = Quaternion.Slerp(player.rotation, target, Time.deltaTime*rotSpeed);
+    }
+}
